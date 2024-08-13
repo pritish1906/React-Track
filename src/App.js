7ed63f747b05387e5253f1369cca482e25fd61b3
Pritish Sharma <pritishsharma96@gmail.com>
2024-08-13 19:28:07 +0530

    Day2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-
-
 import './App.css';
 import Component from './Components/Component';
 import HelloXml from './Components/HelloXml';
 import Props from './Components/Props';
 import List from './Components/List';
 import States from './Components/States';
+import FetchData from './Components/FetchData';
 function App() {
   return (
     <div className="App">
      <HelloXml/>
-     {/* <Component/>
-     <Props name="Pritish" age={21} email="xyz@gmail.com"/>
-     <List/> */}
-     <States/>
+      <FetchData/>
     </div>
   );
 }
