bd05f4c4f9f9357445c0a5643aa96dc4f3203ba6
Pritish Sharma <pritishsharma96@gmail.com>
2024-08-21 19:17:17 +0530

    Day3

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ import Props from './Components/Props';
 import List from './Components/List';
 import States from './Components/States';
 import FetchData from './Components/FetchData';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './Pages/Home';
+import { Profile } from './Pages/Profile';
+import Contact from './Pages/Contact';
+import Navbar from './Components/Navbar';
 function App() {
   return (
     <div className="App">
-     <HelloXml/>
-      <FetchData/>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
